db10182585ead8a56ef84bfdcda0cad804711264
José Rodriguez <joseantonio2199@gmail.com>
2023-06-11 22:36:27 -0500

    home update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
+import home from "../img/home.png";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="neko-home">
       <div className="content">
-        <h1>NEKO</h1>
-        <h3>Welcome, my cat</h3>
-        <p>Now you're team cats, NOT dogs, rabbits... other.</p>
-        <p>We'll keep that in mind</p>
-        <button
-          className="login"
-          onClick={() => {
-            navigate("/login");
-          }}
-        >
-          Log In
-        </button>
-        <button
-          className="signup"
-          onClick={() => {
-            navigate("/signup");
-          }}
-        >
-          Sign Up
-        </button>
+        <div className="welcome">
+          <h1>NEKO</h1>
+          <h3>Welcome, my cat</h3>
+          <h4>Let's start!</h4>
+          <br />
+          <p>Now you're team cats, NOT dogs, rabbits... other.</p>
+          <p>We'll keep that in mind</p>
+        </div>
+
+        <div className="home-buttons">
+          <button
+            className="login"
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Log In
+          </button>
+          <button
+            className="signup"
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
+      <img src={home} alt="home" />
     </div>
   );
 };
